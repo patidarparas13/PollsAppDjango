1e981d185d149f77c7adce455847ae2cd977989a
patidarparas13 <patidarparas13@gmail.com>
2018-10-25 00:12:56 +0530

    Tutorial Part 3 Completed

--- PollsAppDjango/urls.py
+++ PollsAppDjango/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from polls import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),path('<int:question_id>/vote/', views.vote, name='vote'),
 
 ]
